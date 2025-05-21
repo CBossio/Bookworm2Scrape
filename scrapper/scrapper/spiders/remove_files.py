74c937b648e61614ae624695d972f0662f63c56d
CBossio <128101930+CBossio@users.noreply.github.com>
2025-05-21 15:13:18 -0300

    Update remove_files.py

--- scrapper/scrapper/spiders/remove_files.py
+++ scrapper/scrapper/spiders/remove_files.py
@@ -3,8 +3,11 @@ import shutil
 
 #Remove old csv and excels
 def remove_old():
-    current_dir = os.getcwd()
-    export_dir = os.path.join(current_dir, "scrapper", "spiders", "exports")
-    for f in os.listdir(export_dir):
-        if f.endswith(".csv") or f.endswith(".xlsx"):
-            os.remove(os.path.join(export_dir, f))
+    try:
+        current_dir = os.getcwd()
+        export_dir = os.path.join(current_dir, "scrapper", "spiders", "exports")
+        for f in os.listdir(export_dir):
+            if f.endswith(".csv") or f.endswith(".xlsx"):
+                os.remove(os.path.join(export_dir, f))
+    except Exception as e:
+        print("Folder does not exist")
