42b5dc4579fe3d57843f9613589a8efe91f089f6
CBossio <128101930+CBossio@users.noreply.github.com>
2025-05-21 15:14:09 -0300

    Create handle_failure.py

---
+++ scrapper/scrapper/spiders/handle_failure.py
@@ -0,0 +1,15 @@
+import os
+from scrapper.spiders.paths import absolute_paths
+export_dir = absolute_paths()
+
+def handle_failure_function(self, failure, code):
+    request = failure.request
+    proxy = request.meta.get("proxy", "No proxy set")
+    
+    self.logger.warning(
+        f"Request failed or timed out: {request.url} - "
+        f"Reason: {failure.value} - Proxy: {proxy}"
+    )
+    
+    with open(os.path.join(export_dir,f"failed_urls{code}.txt"), "a", encoding="utf-8") as f:
+        f.write(f"{request.url} | Proxy: {proxy}\n")
